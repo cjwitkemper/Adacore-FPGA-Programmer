399fad1e2810b69c05b4c930b3ec16d51f115a68
rhit-nutterkp <nutterkp@rose-hulman.edu>
2026-01-18 19:47:33 -0500

    USART Sends File data

--- UART_Test/src/uart_example.adb
+++ UART_Test/src/uart_example.adb
@@ -5,16 +5,20 @@ with Interfaces; use Interfaces;
 procedure uart_example is
 
    --  Register Definitions
+   --  RCC
    RCC_AHBENR  : Unsigned_32 with Address => To_Address (16#4002_1014#);
    RCC_APB2ENR : Unsigned_32 with Address => To_Address (16#4002_1018#);
    RCC_APB1ENR : Unsigned_32 with Address => To_Address (16#4002_101C#);
+   --  GPIOA
    GPIOA_MODER : Unsigned_32 with Address => To_Address (16#4800_0000#);
    GPIOA_BSRR  : Unsigned_32 with Address => To_Address (16#4800_0018#);
    GPIOA_AFRL  : Unsigned_32 with Address => To_Address (16#4800_0020#);
+   --  SPI1
    SPI1_CR1    : Unsigned_16 with Address => To_Address (16#4001_3000#);
    SPI1_CR2    : Unsigned_16 with Address => To_Address (16#4001_3004#);
    SPI1_SR     : Unsigned_16 with Address => To_Address (16#4001_3008#);
    SPI1_DR     : Unsigned_8  with Address => To_Address (16#4001_300C#);
+   --  USART2
    USART2_CR1  : Unsigned_32 with Address => To_Address (16#4000_4400#);
    USART2_BRR  : Unsigned_32 with Address => To_Address (16#4000_440C#);
    USART2_ISR  : Unsigned_32 with Address => To_Address (16#4000_441C#);
@@ -24,33 +28,31 @@ procedure uart_example is
    RXNE_Flag : constant Unsigned_16 := 16#0001#;
    TXE_Flag : constant Unsigned_16 := 16#0002#;
    BSY_Flag : constant Unsigned_16 := 16#0080#;
-   UART_RXNE : constant Unsigned_32 := 16#0000_0020#; --  Bit 5 in ISR
-
-   Incoming_Byte : Unsigned_8;
+   USART_RXNE : constant Unsigned_32 := 16#0000_0020#; --  Bit 5 in ISR
 
    --  Setting GPIO
    procedure Initialize_Hardware is
    begin
 
-      --  Clocks for GPIOA and SPI1
-      RCC_AHBENR  := RCC_AHBENR or 16#0002_0000#;
+      --  Clocks for GPIOA and SPI1 and USART2
+      RCC_AHBENR  := RCC_AHBENR or 16#0002_0000#; --  GPIOA
       RCC_APB2ENR := RCC_APB2ENR or 16#0000_1000#; --  SPI1 (bit 12)
       RCC_APB1ENR  := RCC_APB1ENR or 16#0002_0000#; --  USART2 (bit 17)
 
       --  Setting pins SPI(PA5,6,7) UART(PA2,PA3)
-      GPIOA_MODER := (GPIOA_MODER and 16#FFFF_003F#) or 16#0000_A9A0#;
+      GPIOA_MODER := (GPIOA_MODER and 16#FFFF_000F#) or 16#0000_A9A0#;
 
       --  Select Alternate function for PA5, PA6, PA7
-      GPIOA_AFRL := (GPIOA_AFRL and 16#000F_FFFF#) or 16#0000_1100#;
+      GPIOA_AFRL := (GPIOA_AFRL and 16#0000_00FF#) or 16#0000_1100#;
 
       --  CR1: Master, Baud Rate
-      SPI1_CR1    := 16#0304#; --  Master, 256
+      SPI1_CR1    := 16#033C#; --  Master, 256
       SPI1_CR2    := 16#0700# or 16#1000#; --  8-bit mode
       SPI1_CR1    := SPI1_CR1 or 16#0040#; --  Enable
 
       --  UART Config (115200 Baud @ 8MHz Clock)
       --  USART2_BRR := 16#0045#; --  8,000,000 / 115,200 = 69 (0x45)
-      USART2_BRR := 16#01A1#; -- 48,000,000 / 115,200 = 417 (0x1A1)
+      USART2_BRR := 16#01A1#; --  48,000,000 / 115,200 = 417 (0x1A1)
       USART2_CR1 := 16#0000_000D#;
 
       --  Set CS High initially
@@ -60,46 +62,60 @@ procedure uart_example is
    --  Checks sends and receives
    function Transceive (Data_Out : Unsigned_8) return Unsigned_8 is
    begin
-      --  Wait for TX buffer to be empty
       while (SPI1_SR and TXE_Flag) = 0 loop null; end loop;
-
-      --  Send byte
       SPI1_DR := Data_Out;
-
-      --  Wait for RX buffer to have data (Arduino response)
       while (SPI1_SR and RXNE_Flag) = 0 loop null; end loop;
       return SPI1_DR;
    end Transceive;
+
+   function Data_Available_UART return Boolean is
+   begin
+      return (USART2_ISR and USART_RXNE) /= 0;
+   end Data_Available_UART;
+
+   function Receive_UART return Unsigned_8 is
+   begin
+      return USART2_RDR;
+   end Receive_UART;
+
+   Incoming_Byte : Unsigned_8;
+   Trash         : Unsigned_8;
+   In_Transfer   : Boolean := False;
+   Timeout_Count : Natural := 0;
 
    --  Main loop
 begin
    Initialize_Hardware;
 
    loop
-      --  Check if a byte arrived from Ubuntu via the USB cable
-      if (USART2_ISR and UART_RXNE) /= 0 then
+      if Data_Available_UART then
+         Incoming_Byte := Receive_UART;
 
-         --  Read the file byte from USART2
-         Incoming_Byte := USART2_RDR;
+         --  If this is the first byte of a stream, pull CS low
+         if not In_Transfer then
+            GPIOA_BSRR := 16#0010_0000#; -- CS Low (PA4)
+            In_Transfer := True;
+         end if;
 
-         --  SEND TO ARDUINO
-         GPIOA_BSRR := 16#0010_0000#; --  CS LOW
+         --  Forward to SPI
+         Trash := Transceive (Incoming_Byte);
 
-         declare
-            Trash : Unsigned_8;
-         begin
-            Trash := Transceive (Incoming_Byte);
-         end;
+         --  Reset timeout because we are active
+         Timeout_Count := 0;
+      else
+         --  If we were in a transfer but no data is coming
+         if In_Transfer then
+            Timeout_Count := Timeout_Count + 1;
+            --  Small artificial delay to define "End of File"
+            if Timeout_Count > 50_000 then
+               --  Wait for SPI to finish last bits
+               while (SPI1_SR and BSY_Flag) /= 0 loop null; end loop;
 
-         --  Ensure SPI is finished before lifting CS
-         while (SPI1_SR and BSY_Flag) /= 0 loop null; end loop;
-
-         GPIOA_BSRR := 16#0000_0010#; --  CS HIGH
-         
-         --  Small delay to let Arduino process the byte
-         for J in 1 .. 50_000 loop
-            null;
-         end loop;
+               GPIOA_BSRR := 16#0000_0010#; -- CS High (PA4)
+               In_Transfer := False;
+               Timeout_Count := 0;
+            end if;
+         end if;
       end if;
    end loop;
 
