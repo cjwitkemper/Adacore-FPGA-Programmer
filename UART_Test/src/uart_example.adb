fb13235dcb430d9e1817285c7ab519fa24e8bb6c
rhit-nutterkp <nutterkp@rose-hulman.edu>
2026-02-06 10:03:53 -0500

    Added Procedure for CS High & Tranceive_File

--- UART_Test/src/uart_example.adb
+++ UART_Test/src/uart_example.adb
@@ -38,9 +38,9 @@ procedure uart_example is
       GPIOB_Periph.MODER := (As_Array => True,
                         Arr      => (3 | 4 | 5 => 1, others => 0));
       --  PI1 Configuration (SVD Record)
-      --  CR1: Master mode, Baud rate div 256, Software Slave Mgmt, Internal Slave Select
+      --  CR1: Master mode, Baud rate 12MHz, Software Slave Mgmt, Internal Slave Select
       SPI1_Periph.CR1 := (MSTR     => 1,
-                          BR       => 7, --  F_PCLK / 256
+                          BR       => 1,
                           LSBFIRST => 0,
                           SSM      => 1,
                           SSI      => 1,
@@ -54,14 +54,15 @@ procedure uart_example is
 
       -- USART2 Configuration (115200 Baud @ 48MHz)
       -- 16#01A1# -> Mantissa 26 (16#1A#), Fraction 1
-      USART2_Periph.BRR := (DIV_Mantissa => 16#1A#,
-                            DIV_Fraction => 1,
+      USART2_Periph.BRR := (DIV_Mantissa => 16#18#,
+                            DIV_Fraction => 0,
                             others       => <>);
 
       -- Enable UART, Transmit, and Receive
       USART2_Periph.CR1 := (UE     => 1,
                             TE     => 1,
                             RE     => 1,
+                            OVER8  => 1,
                             others => <>);
 
       --  Initial CS High (PA4)
@@ -71,18 +72,21 @@ procedure uart_example is
 
    --  Checks sends and receives
    function Transceive (Data_Out : Byte) return Byte is
-   --  Dummy : Byte;
    DR_8 : Byte with Address => SPI1_Periph.DR'Address;
    begin
       while SPI1_Periph.SR.TXE = 0 loop null; end loop;
-      --  SPI1_Periph.DR.DR := DR_DR_Field (Data_Out);
       DR_8 := Data_Out;
       while SPI1_Periph.SR.RXNE = 0 loop null; end loop;
-      --  Dummy := Byte (SPI1_Periph.DR.DR);
-      --  return Byte (SPI1_Periph.DR.DR);
       return DR_8;
-      --  return Dummy;
    end Transceive;
+
+   function Transceive_File (Data_Out : Byte) return Byte is
+   DR_8 : Byte with Address => SPI1_Periph.DR'Address;
+   begin
+      while SPI1_Periph.SR.TXE = 0 loop null; end loop;
+      DR_8 := Data_Out;
+      return DR_8;
+   end Transceive_File;
 
    function Data_Available_UART return Boolean is
    begin
@@ -94,6 +98,18 @@ procedure uart_example is
       return Byte (USART2_Periph.RDR.RDR);
    end Receive_UART;
 
+   procedure CS_Low is
+   begin
+      GPIOA_Periph.BSRR := (BS => (As_Array => True, Arr => (others => 0)),
+                            BR => (As_Array => True, Arr => (4 => 1, others => 0)));
+   end CS_Low;
+
+   procedure CS_High is
+   begin
+      GPIOA_Periph.BSRR := (BS => (As_Array => True, Arr => (4 => 1, others => 0)),
+                            BR => (As_Array => True, Arr => (others => 0)));
+   end CS_High;
+
    Incoming_Byte : Byte;
    Trash         : Byte;
    In_Transfer   : Boolean := False;
@@ -103,9 +119,7 @@ procedure uart_example is
    procedure Send_Command (Cmd : Byte) is
       Dummy : Byte;
    begin
-      -- CS Low (PA4)
-      GPIOA_Periph.BSRR := (BR => (As_Array => True, Arr => (4 => 1, others => 0)),
-                            BS => (As_Array => True, Arr => (others => 0)));
+      CS_Low;
       
       Dummy := Transceive (Cmd);
       Dummy := Transceive (16#00#);
@@ -113,9 +127,7 @@ procedure uart_example is
       -- Wait for SPI to finish last bit before raising CS
       while SPI1_Periph.SR.BSY /= 0 loop null; end loop;
 
-      -- CS High (PA4)
-      GPIOA_Periph.BSRR := (BS => (As_Array => True, Arr => (4 => 1, others => 0)),
-                            BR => (As_Array => True, Arr => (others => 0)));
+      CS_High;
    end Send_Command;
 
    --  Main loop
@@ -123,43 +135,43 @@ begin
    Initialize_Hardware;
    delay until Ada.Real_Time.Clock + Milliseconds(100);
  
-   -- 1. Read Status (0x41) + 7 dummy bytes to fulfill 64-bit cycle in chart
-   GPIOA_Periph.BSRR := (BR => (As_Array => True, Arr => (4 => 1, others => 0)),
-                         BS => (As_Array => True, Arr => (others => 0)));
+   --  Read Status (0x41) + 7 dummy bytes to fulfill 64-bit cycle in chart
+   CS_Low;
    Trash := Transceive (16#41#);
    for I in 1 .. 7 loop Trash := Transceive (16#00#); end loop;
    while SPI1_Periph.SR.BSY /= 0 loop null; end loop;
-   GPIOA_Periph.BSRR := (BS => (As_Array => True, Arr => (4 => 1, others => 0)),
-                         BR => (As_Array => True, Arr => (others => 0)));
+   CS_High;
    delay until Ada.Real_Time.Clock + Milliseconds(1);
 
-   -- 2. Read ID (0x11) + 7 dummy bytes
-   GPIOA_Periph.BSRR := (BR => (As_Array => True, Arr => (4 => 1, others => 0)),
-                         BS => (As_Array => True, Arr => (others => 0)));
+   --  Read ID (0x11) + 7 dummy bytes
+   CS_Low;
    Trash := Transceive (16#11#);
    for I in 1 .. 7 loop Trash := Transceive (16#00#); end loop;
    while SPI1_Periph.SR.BSY /= 0 loop null; end loop;
-   GPIOA_Periph.BSRR := (BS => (As_Array => True, Arr => (4 => 1, others => 0)),
-                         BR => (As_Array => True, Arr => (others => 0)));
+   CS_High;
    delay until Ada.Real_Time.Clock + Milliseconds(1);
-   --  3. Write Enable (0x15)
+
+   --  Init Adress
+   Send_Command (16#12#);
+   delay until Ada.Real_Time.Clock + Milliseconds(1);
+   
+   --  Write Enable (0x15)
    Send_Command (16#15#);
    delay until Ada.Real_Time.Clock + Milliseconds(1);
+
    loop
       if Data_Available_UART then
          Incoming_Byte := Receive_UART;
 
          --  If this is the first byte of a stream, pull CS low
          if not In_Transfer then
-            -- CS Low (PA4)
-            GPIOA_Periph.BSRR := (BS => (As_Array => True, Arr => (others => 0)),
-                                  BR => (As_Array => True, Arr => (4 => 1, others => 0)));
+            CS_Low;
             Trash := Transceive (16#3B#); --  Write Data
             In_Transfer := True;
          end if;
 
          --  Forward to SPI
-         Trash := Transceive (Incoming_Byte);
+         Trash := Transceive_File (Incoming_Byte);
 
          --  Reset timeout because we are active
          Timeout_Count := 0;
@@ -172,12 +184,10 @@ begin
                --  Wait for SPI to finish last bits
                while SPI1_Periph.SR.BSY /= 0 loop null; end loop;
 
-               -- CS High (PA4)
-               GPIOA_Periph.BSRR := (BS => (As_Array => True, Arr => (4 => 1, others => 0)),
-                                     BR => (As_Array => True, Arr => (others => 0)));
+               CS_High;
                
                Send_Command (16#3A#); --  Write Disable
-               delay until Ada.Real_Time.Clock + Milliseconds(5);
+               delay until Ada.Real_Time.Clock + Milliseconds(1);
                In_Transfer := False;
                Timeout_Count := 0;
             end if;
