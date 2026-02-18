61f5399b0ea7a193a1bc1eb62cd0804f07148b48
rhit-nutterkp <nutterkp@rose-hulman.edu>
2026-02-17 23:16:14 -0500

    Renaming Folders

--- SPI_Test/tests/common/stm32f070rb_blinky_tests.ads
+++
@@ -1,8 +0,0 @@
-pragma Warnings (Off);
-with Ada.Assertions; use Ada.Assertions;
---  Make Assert visible to children
-pragma Warnings (On);
-
-package Stm32f070rb_Blinky_Tests is
-
-end Stm32f070rb_Blinky_Tests;
