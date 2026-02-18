02b079eb0d48c4e5b7fd4e632697e692a29bd370
rhit-nutterkp <nutterkp@rose-hulman.edu>
2026-02-17 22:24:07 -0500

    USART added for file transfer

--- JTAG_Test/src/jtag_test.adb
+++ JTAG_Test/src/jtag_test.adb
@@ -2,6 +2,7 @@ with Interfaces;     use Interfaces;
 with STM32F0x0;      use STM32F0x0;
 with STM32F0x0.RCC;  use STM32F0x0.RCC;
 with STM32F0x0.GPIO; use STM32F0x0.GPIO;
+with STM32F0x0.USART; use STM32F0x0.USART;
 with Ada.Real_Time;  use Ada.Real_Time;
 
 procedure jtag_test is
@@ -20,19 +21,32 @@ procedure jtag_test is
       --  Enable GPIOA
       RCC_Periph.AHBENR.IOPAEN := 1;
 
+      --  Enable USART2
+      RCC_Periph.APB1ENR.USART2EN := 1;
+
       --  PA4, PA5, PA6, PA7
-      GPIOA_Periph.MODER :=
-        (As_Array => True,
-         Arr      =>
-           (6         => 0,
-            -- PA6 as Input
-            4 | 5 | 7 => 1,
-            others    => 0));
+      GPIOA_Periph.MODER.Arr (4) := 1;
+      GPIOA_Periph.MODER.Arr (5) := 1;
+      GPIOA_Periph.MODER.Arr (6) := 0;
+      GPIOA_Periph.MODER.Arr (7) := 1;
 
       --  Initial CS Low(PA4) and TCK, TMS, TDI Low
-      GPIOA_Periph.BSRR :=
-        (BS => (As_Array => True, Arr => (others => 0)),
-         BR => (As_Array => True, Arr => (4 | 5 | 6 | 7 => 1, others => 0)));
+      GPIOA_Periph.BSRR.BR.Arr (4) := 1;
+      GPIOA_Periph.BSRR.BR.Arr (5) := 1;
+      GPIOA_Periph.BSRR.BR.Arr (6) := 1;
+      GPIOA_Periph.BSRR.BR.Arr (7) := 1;
+
+      --  USART2 Configuration (115200 Baud @ 48MHz)
+      USART2_Periph.BRR := (DIV_Mantissa => 16#416#,
+                            DIV_Fraction => 0,
+                            others       => <>);
+
+      --  Enable UART, Transmit, and Receive
+      USART2_Periph.CR1 := (UE     => 1,
+                            TE     => 1,
+                            RE     => 1,
+                            OVER8  => 0,
+                            others => <>);
 
    end Initialize_Hardware;
 
@@ -121,14 +135,14 @@ procedure jtag_test is
 
    end Send_Command;
 
-   procedure Transceive_Byte_JTAG (Data_Out : Byte) is
+   procedure Transceive_Byte_JTAG (Data_Out : Byte; Last_Byte : Boolean) is
       TDO_Byte : Byte := 0;
    begin
       for Bit in 0 .. 7 loop
-         if(Bit = 7) then
+         if Last_Byte and then (Bit = 7) then
             Set_TMS_Pin (1); -- Pull TMS high on the last bit to exit Shift-DR
-
          end if;
+
          if (Data_Out and Shift_Left (1, Bit)) /= 0 then
             Pin_High (TDI_Pin);
          else
@@ -138,6 +152,16 @@ procedure jtag_test is
          Pulse_TCK;
       end loop;
    end Transceive_Byte_JTAG;
+
+   function Data_Available_UART return Boolean is
+   begin
+      return USART2_Periph.ISR.RXNE /= 0;
+   end Data_Available_UART;
+
+   function Receive_UART return Byte is
+   begin
+      return Byte (USART2_Periph.RDR.RDR);
+   end Receive_UART;
 
    procedure TDO_Test is
 
@@ -215,21 +239,41 @@ procedure jtag_test is
       Set_TMS_Pin (0);
       Pulse_TCK; -- CAPTURE-DR
       Pulse_TCK; -- Shift-DR
-
-      Transceive_Byte_JTAG (16#AB#); -- Send a byte and read the response
-
    end TDO_Test;
 
+   First_Byte : Byte;
+   Second_Byte : Byte;
+   Byte_Count : Natural := 0;
+   In_Transfer : Boolean := False;
+   Timeout_Count : Natural := 0;
 begin
    Initialize_Hardware;
 
    TDO_Test;
-   --  Ada.Text_IO.Put_Line(Ada.Real_Time.Clock);
-   --  JTAG_Sequence (TMS_Sequence, TDI_Sequence, TDI_Clk, 8);
-   --  Read_ID_Final;
-   --  System_Clock : = HAL.RCC.SystemCoreClock;
-   --  Ada.Text_IO.Put_Line("System Clock: " 7Integer'im)
+
    loop
-      null;
+      if Data_Available_UART then
+         First_Byte := Second_Byte;
+         Second_Byte := Receive_UART;
+         Byte_Count := Byte_Count + 1;
+         if not In_Transfer then
+            In_Transfer := True;
+         end if;
+
+         if Byte_Count > 1 then
+            Transceive_Byte_JTAG (First_Byte, False);
+         end if;
+         Timeout_Count := 0;
+      else
+         if In_Transfer then
+            Timeout_Count := Timeout_Count + 1;
+            if Timeout_Count > 50_000 then
+               In_Transfer := False;
+               Byte_Count := 0;
+               Transceive_Byte_JTAG (Second_Byte, True);
+            end if;
+         end if;
+      end if;
+
    end loop;
 end jtag_test;
